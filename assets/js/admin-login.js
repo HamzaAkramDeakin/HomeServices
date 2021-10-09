2c4606f1bffe4556479c7e2e94c20b1d945fe524
Hamza Akram <hamzaakram66@gmail.com>
2021-10-09 20:14:50 +0530

    Added signup back-end

--- assets/js/admin-login.js
+++ assets/js/admin-login.js
@@ -1,7 +1,26 @@
 /**
  * Login
  */
+
+// Handle login
 function handleLogin(event) {
-  // Prevent page reload
+   
+  // Prevent reloading the page
   event.preventDefault();
+
+  // Form data
+  const username = document.querySelector('#username')
+  const password = document.querySelector('#password')
+
+  // Check the credentials
+  if (username.value === 'admin' && password.value === '123') {
+    window.location = '/admin/';
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Incorrect Credentials',
+      text: 'Username or password is incorrect',
+    })
+  }
 }
+
--- assets/js/customer-signup.js
+++ assets/js/customer-signup.js
@@ -1,7 +1,50 @@
 /**
  * Sign up
  */
+
 function handleSignup(event) {
   // Prevent page reload
   event.preventDefault();
+
+  // To grab all html elements
+  const firstName = document.querySelector("#firstName");
+  const lastName = document.querySelector("#lastName");
+  const email = document.querySelector("#email");
+  const password = document.querySelector("#password");
+
+  // To handle the post request
+  fetch("/api/customer", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+      password: password.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.error) {
+        Swal.fire({
+          icon: "error",
+          title: "Incorrect Credentials",
+          text: data.error,
+        });
+      } else {
+        Swal.fire({
+          icon: "success",
+          title: "Account Created",
+          text: "You can go ahead and sign in",
+        });
+      }
+    })
+    .catch((ex) => {
+      toast.error(ex.message);
+    })
+    .finally(() => {
+      event.target.reset();
+    });
 }
