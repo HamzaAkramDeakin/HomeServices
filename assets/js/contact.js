9ea6d3dc4ef22607d938e0b893aaf6d9748304cf
Hamza Akram <hamzaakram66@gmail.com>
2021-10-10 15:36:45 +0530

    Added Contact page back-end

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,4 +4,40 @@
 function handleSend(event) {
   // Prevent page reload
   event.preventDefault();
+
+  const name = document.querySelector("#name");
+  const email = document.querySelector("#email");
+  const message = document.querySelector("#message");
+
+  fetch("/api/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.error) {
+        Swal.fire({
+          icon: "error",
+          title: "Something went wrong",
+          text: data.error,
+        });
+      } else {
+        Swal.fire({
+          icon: "success",
+          showConfirmButton:true,
+          title: "Form Submitted Successfully",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location = '/';
+          }
+        })
+      }
+    });
 }
